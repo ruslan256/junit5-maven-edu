8b5ceef4ad9a974a878195eeafa97734ae521468
ruslan256 <github.rus.yan@mail.ru>
2022-12-17 18:21:11 +0800

    feat: добавить слой для работы с Launcher API, пример работы с SummaryGeneratingListener

---
+++ src/test/java/com/ruslan/edu/junit5/TestLauncher.java
@@ -0,0 +1,33 @@
+package com.ruslan.edu.junit5;
+
+//import com.ruslan.edu.junit5.service.UserServiceTest;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+
+public class TestLauncher {
+
+    public static void main(String[] args) {
+        Launcher launcher = LauncherFactory.create();
+//        launcher.registerLauncherDiscoveryListeners();
+        SummaryGeneratingListener summaryGeneratingListener = new SummaryGeneratingListener();
+//        launcher.registerTestExecutionListeners(summaryGeneratingListener);
+
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder
+                .request()
+//                .selectors(DiscoverySelectors.selectClass(UserServiceTest.class))
+                .selectors(DiscoverySelectors.selectPackage("com.ruslan.edu.junit5.service"))
+                .build();
+
+        launcher.execute(request, summaryGeneratingListener);
+
+        try (PrintWriter writer = new PrintWriter(System.out)) {
+            summaryGeneratingListener.getSummary().printTo(writer);
+        }
+    }
+}
